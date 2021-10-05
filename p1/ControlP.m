3a146ea5dfbd87382889af485fbf84c14a984c32
Quintanaaalberto <79100197+Quintanaaalberto@users.noreply.github.com>
2021-10-05 18:17:53 +0200

    Update ControlP.m

--- p1/ControlP.m
+++ p1/ControlP.m
@@ -42,33 +42,33 @@ Crd = Cd;
 % G = Pm*K;
 
 %% CONTROL pid
-% 
-% wo_pid = 1.4*wo_p;
-% Fpi=-10;
-% f=0.1;
-% b=1;
-% 
-% 
-% Fic = -180+Fm-angle(freqresp(Pam,wo_pid))*(180/pi);
-% Fpd=Fic-Fpi;
-% 
-% Ac=1/abs(freqresp(Pam, wo_pid));
-% 
-% fmax=(1-sind(Fpd))/(1+sind(Fpd));
-% if f>fmax
-%     f=fmax;
-% end
-% 
-% % formato serie
-% D=((1/f-1)/(2*tand(Fpd))-sqrt(((1/f-1)/(2*(tand(Fpd))))^2-1/f))/wo_pid;
-% Kp=Ac*cosd(Fpi)*(sqrt(1+(f*wo_pid*D)^2))/(sqrt(1+(wo_pid*D)^2));
-% 
-% % formato paralelo
-% mu=1+(1-f)*D/I;
-% K=mu*Kp;
-% Td=((1/mu)-f)*D;
-% Ti=mu*I;
-% N=(1/(mu*f))-1;
-% 
-% C_pid=K*(1+1/(Ti*s)+(Td*s)/(1+Td*s/N));
-% Cr_pid=K*(b+1/(Ti*s)+(Td*s)/(1+Td*s/N));
+
+wo_pid = 1.7*wo_p;
+Fpi=-10;
+f=0.1;
+b=1;
+
+
+Fic = -180+Fm-angle(freqresp(Pm,wo_pid))*(180/pi);
+Fpd=Fic-Fpi;
+
+Ac=1/abs(freqresp(Pm, wo_pid));
+
+fmax=(1-sind(Fpd))/(1+sind(Fpd));
+if f>fmax
+    f=fmax;
+end
+
+% formato serie
+D=((1/f-1)/(2*tand(Fpd))-sqrt(((1/f-1)/(2*(tand(Fpd))))^2-1/f))/wo_pid;
+Kp=Ac*cosd(Fpi)*(sqrt(1+(f*wo_pid*D)^2))/(sqrt(1+(wo_pid*D)^2));
+
+% formato paralelo
+mu=1+(1-f)*D/I;
+K=mu*Kp;
+Td=((1/mu)-f)*D;
+Ti=mu*I;
+N=(1/(mu*f))-1;
+
+C_pid=K*(1+1/(Ti*s)+(Td*s)/(1+Td*s/N));
+Cr_pid=K*(b+1/(Ti*s)+(Td*s)/(1+Td*s/N));
