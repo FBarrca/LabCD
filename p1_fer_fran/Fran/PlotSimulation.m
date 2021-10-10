9a1eae49ec6e367607cdf099f04682675f0dc2ef
FranciscoBarCa <60709314+FranciscoBarCa@users.noreply.github.com>
2021-10-10 12:08:34 -0700

    mejora siimulacion

--- p1_fer_fran/Fran/PlotSimulation.m
+++ p1_fer_fran/Fran/PlotSimulation.m
@@ -1,7 +1,9 @@
 function [result] = PlotSimulation(run_name,sim_name)
 %Simulink.sdi.loadView('defaultsignalconfig.mldatx')
 Simulink.sdi.setRunNamingRule(run_name+' <run_index> <model_name>');
-result=plot(sim(sim_name))
-Simulink.sdi.loadView('defaultsignalconfig.mldatx');
+result=sim(sim_name)
+plot(result)
+Simulink.sdi.view
+%Simulink.sdi.loadView('defaultsignalconfig.mldatx');
 end
 
--- p1_fer_fran/Fran/RunSIM_VELOCIDAD.m
+++
@@ -1,6 +0,0 @@
-function [] = RunSIM_VELOCIDAD(Crd,Cd)
-load v_Velocidad_rampa.mat Velocidad
-%sim("Sim_Velocidad")
-PlotSimulation("Simulacion","SIM_VELOCIDAD.slx")
-end
-
