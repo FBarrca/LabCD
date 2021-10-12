f9182886ff592ffe54b97b3e1a58fe794a3308e7
juaco99 <79275363+juaco99@users.noreply.github.com>
2021-10-12 17:39:18 +0200

    Pulsacion y pico de resonancia en control P

--- p1_al_joa/pruebaLab/scriptPruebaP.m
+++ p1_al_joa/pruebaLab/scriptPruebaP.m
@@ -52,5 +52,9 @@ ws_p=fminsearch(@(w) -abs(freqresp(S_p,w)),1);
 Ms_p=20*log10(abs(freqresp(S_p,ws_p)));
 % Respuesta temporal a analizar en ltiview
 
+% Pulsacion de resonancia
+wr_p=fminsearch(@(w) -abs(freqresp(F_p,w)),1);
+% Pico de resonancia
+Mr_p=20*log10(abs(freqresp(F_p,wr_p)))-20*log10(dcgain(F_p));
 
 
